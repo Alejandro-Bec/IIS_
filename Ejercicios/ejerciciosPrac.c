e922c6ef8241bd9304515753f9334affa8638f25
Alejandro-Bec <danieljavier799@gmail.com>
2025-11-12 10:47:51 -0600

    Ejercicios agregados

---
+++ Ejercicios/ejerciciosPrac.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float llenarCalificaciones(int n);
+void mostrarCalificaciones(float *arr, int n);
+float calcularPromedio(float *arr, int n);
+
+int main(){
+
+  int tam;
+  float *calificaciones;
+  
+  printf("Cuantos calificaciones deseas registrar?: ");
+  scanf("%d", &tam);
+  
+  calificaciones = llenarCalificaciones(tam);
+  
+  free(calificaciones);
+  return 0;
+
+}
+
+float llenarCalificaciones(int n){
+
+ float arr = (float *) malloc(n * sizeof(float));
+  if(arr == NULL){
+    printf("Error: no se pudo reservar memoria.");
+    return(1);
+  }
+  
+  for(int i = 0; i < n; i++){
+    printf("Ingresa la calificacion del alumno %d:  \n", i + 1);
+    scanf("%f", &arr[i]);
+  }
+}
---
+++ Ejercicios/estructuras.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Contacto {
+  char nombre[50];
+  char numero[15];
+  char correo[30];
+  int edad;
+}
+
+void capturarContacto();
+void imprimirContacto(con1, con2);
+
+int main(){
+
+  struct Contacto c1, c2;
+  int n;
+  Contacto contacto[i];
+  
+  printf("Cuantos contactos quieres agregar?: ");
+  scanf("%d", &n);
+  
+  c1 = capturarContacto();
+  
+
+
+
+
+}
+
